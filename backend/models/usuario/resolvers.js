2c4a6d242352b470c70de9e233d0c2f92f283920
cado10 <candresdelgado@gmail.com>
2021-12-05 21:34:53 -0500

    adding user details and advances features

--- backend/models/usuario/resolvers.js
+++ backend/models/usuario/resolvers.js
@@ -1,58 +1,67 @@
 import { UserModel } from './usuario.js';
+import bcrypt from 'bcrypt';
+import { InscriptionModel } from '../inscripcion/inscripcion.js';
 
 const resolversUsuario = {
-  Query: {
-    Usuarios: async (parent, args) => {
-      console.log('parent usuario', parent);
-      const usuarios = await UserModel.find();
-      return usuarios;
-    },
-    Usuario: async (parent, args) => {
-      const usuario = await UserModel.findOne({ _id: args._id });
-      return usuario;
-    },
-  },
-  Mutation: {
-    crearUsuario: async (parent, args) => {
-      const usuarioCreado = await UserModel.create({
-        nombre: args.nombre,
-        apellido: args.apellido,
-        identificacion: args.identificacion,
-        correo: args.correo,
-        rol: args.rol,
-      });
-
-      if (Object.keys(args).includes('estado')) {
-        usuarioCreado.estado = args.estado;
-      }
-
-      return usuarioCreado;
-    },
-    editarUsuario: async (parent, args) => {
-      const usuarioEditado = await UserModel.findByIdAndUpdate(
-        args._id,
-        {
-          nombre: args.nombre,
-          apellido: args.apellido,
-          identificacion: args.identificacion,
-          correo: args.correo,
-          estado: args.estado,
+    Usuario: {
+        inscripciones: async (parent, args, context) => {
+            return InscriptionModel.find({ estudiante: parent._id });
         },
-        { new: true }
-      );
+    },
+    Query: {
+        Usuarios: async (parent, args, context) => {
+            const usuarios = await UserModel.find();
+            return usuarios;
+        },
+        Usuario: async (parent, args) => {
+            const usuario = await UserModel.findOne({ _id: args._id });
+            return usuario;
+        },
+    },
+    Mutation: {
+        crearUsuario: async (parent, args) => {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(args.password, salt);
+            const usuarioCreado = await UserModel.create({
+                nombre: args.nombre,
+                apellido: args.apellido,
+                identificacion: args.identificacion,
+                correo: args.correo,
+                rol: args.rol,
+                password: hashedPassword,
+            });
 
-      return usuarioEditado;
+            if (Object.keys(args).includes('estado')) {
+                usuarioCreado.estado = args.estado;
+            }
+
+            return usuarioCreado;
+        },
+        editarUsuario: async (parent, args) => {
+            const usuarioEditado = await UserModel.findByIdAndUpdate(
+                args._id,
+                {
+                    nombre: args.nombre,
+                    apellido: args.apellido,
+                    identificacion: args.identificacion,
+                    correo: args.correo,
+                    estado: args.estado,
+                },
+                { new: true }
+            );
+
+            return usuarioEditado;
+        },
+        eliminarUsuario: async (parent, args) => {
+            if (Object.keys(args).includes('_id')) {
+                const usuarioEliminado = await UserModel.findOneAndDelete({ _id: args._id });
+                return usuarioEliminado;
+            } else if (Object.keys(args).includes('correo')) {
+                const usuarioEliminado = await UserModel.findOneAndDelete({ correo: args.correo });
+                return usuarioEliminado;
+            }
+        },
     },
-    eliminarUsuario: async (parent, args) => {
-      if (Object.keys(args).includes('_id')) {
-        const usuarioEliminado = await UserModel.findOneAndDelete({ _id: args._id });
-        return usuarioEliminado;
-      } else if (Object.keys(args).includes('correo')) {
-        const usuarioEliminado = await UserModel.findOneAndDelete({ correo: args.correo });
-        return usuarioEliminado;
-      }
-    },
-  },
 };
 
 export { resolversUsuario };
